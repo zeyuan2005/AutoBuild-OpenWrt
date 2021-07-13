16ee7ee7cb98ef2639aa48cac0c5c986deaf7675
zeyuan2005 <53356611+zeyuan2005@users.noreply.github.com>
2021-07-13 09:36:26 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,6 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.82.1/192.168.82.41/g' openwrt/package/base-files/files/bin/config_generate
 #2. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
